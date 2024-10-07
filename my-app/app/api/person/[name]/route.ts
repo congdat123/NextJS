0f905647dc6daf3c91409546d6fd6609fefd4eae
dat <cnd21062001@gmail.com>
2024-10-07 17:10:13 +0700

    [test]

--- my-app/app/api/person/[name]/route.ts
+++ my-app/app/api/person/[name]/route.ts
@@ -1,14 +1,14 @@
-import { createKysely } from '@vercel/postgres-kysely';
+// import { createKysely } from '@vercel/postgres-kysely';
 
-export interface Persons {
-    name: string,
-    id: number
-}
+// export interface Persons {
+//     name: string,
+//     id: number
+// }
 
-const db = createKysely<any>();
+// const db = createKysely<any>();
 
-export async function GET(request: Request, context: any) {
-  console.log(context)
-  const res = await db.selectFrom('about').selectAll().where('name', '~', context.params.name).execute() as Persons[];
-  return Response.json(res)
-}
+// export async function GET(request: Request, context: any) {
+//   console.log(context)
+//   const res = await db.selectFrom('about').selectAll().where('name', '~', context.params.name).execute() as Persons[];
+//   return Response.json(res)
+// }
